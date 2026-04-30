4aa8303d2490d1acbc79d9d38e564caa9a77ed64
Robin Stumm <robin.stumm@iohk.io>
2026-04-30 15:56:59 +0200

    drop support for x86_64-darwin
    Nixpkgs expects to drop support for intel macs in 26.11.

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
           targetSystem: import ./nix/internal.nix {inherit inputs targetSystem;}
         );
 
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
       perSystem = {system, ...}: let
         internal = inputs.self.internal.${system};
       in {
